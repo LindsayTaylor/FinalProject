9baca7aaefe06ca4024da37e92684933e92bf780
LindsayTaylor <LindsayNyberg@Lindsays-MacBook-Pro.local>
2015-08-12 19:47:23 -0400

    First draft

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  /*$(window).load(function(){
+     $('#imageID').fadeOut('slow',function(){
+           setTimeout(function(){
+              $(this).fadeOut('slow');*/
+
+              function practice (){
+
+              }
+
+              $("#container").fadeIn(7000);
+
+  });
