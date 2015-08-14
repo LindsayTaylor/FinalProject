846339ffb0daa9df960efe8551047bc8ed2befe2
LindsayTaylor <LindsayNyberg@Lindsays-MBP.home>
2015-08-14 18:35:29 -0400

    2.3
    Added colors to the rest of the songs

--- music.js
+++ music.js
@@ -21,16 +21,39 @@ $(document).ready(function(){
                 $("#grindin").css("color","rgb(255,200,255)");
                 $(".nav li a").css("color","rgb(255,20,147)");
                 $(".nav li").css("border","rgb(255,20,147)");
+                $("footer").css("display","none");
               }
 
               function green(){
                 $("body").css("backgroundImage","none");
-                $("body").css("background-color", "rgb(255,20,147)");
-                $(".music").css("color","rgb(255,20,147)");
-                $("#grindin").css("color","rgb(255,200,255)");
-                $("#header").css("display","none");
+                $("body").css("background-color", "rgb(50,205,50)");
+                $(".music").css("color","rgb(50,205,50)");
+                $("#pop").css("color","rgb(48,128,20)");
+                $(".nav li a").css("color","rgb(50,205,50)");
+                $(".nav li").css("border","rgb(50,205,50)");
+                $("footer").css("display","none");
               }
 
+              function blue(){
+                $("body").css("backgroundImage","none");
+                $("body").css("background-color", "rgb(0,255,255)");
+                $(".music").css("color","rgb(0,255,255)");
+                $("#money").css("color","rgb(0,128,128)");
+                $(".nav li a").css("color","rgb(0,255,255)");
+                $(".nav li").css("border","rgb(0,255,255)");
+                $("footer").css("display","none");
+              }
+
+              function orange(){
+                $("body").css("backgroundImage","none");
+                $("body").css("background-color", "rgb(255,128,0)");
+                $(".music").css("color","rgb(255,128,0)");
+                $("#move").css("color","rgb(205,102,0)");
+                $(".nav li a").css("color","rgb(255,128,0)");
+                $(".nav li").css("border","rgb(255,128,0)");
+                $("footer").css("display","none");
+
+              }
 
               function normal(){
                 $("body").css("backgroundImage","");
@@ -39,12 +62,17 @@ $(document).ready(function(){
                 $("#grindin").css("color","");
                 $(".nav li a").css("color","");
                 $(".nav li").css("border","");
+                $("footer").css("display","");
               }
 
 
-              $("#pop p").mouseenter(green);
+
               $("#grindin p").mouseenter(pink);
+              $("#pop p").mouseenter(green);
+              $("#money p").mouseenter(blue);
+              $("#move p").mouseenter(orange);
               $(".music p").mouseleave(normal);
+              $("grindin p").click(songA);
 
 
   });
