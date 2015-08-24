c01a197848e2192a3b950a009c299b915d1b9c0b
LindsayTaylor <LindsayNyberg@Lindsays-MacBook-Pro.local>
2015-08-23 20:15:57 -0400

    4.2
    Added sound to landing page.  Need to fix play/pause functions.

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
               $("#cover").fadeOut(7000);
               $("#container").fadeIn(500);
               $("#cover").css("display","none");
+
+              function song(){
+                alert("yep");
+              }
+
+              $("h1").click(song);
+              $("img").click(song);
+
+  });
+  var widget = SC.Widget(document.getElementById('song'));
+  $('h1').click(function() {
+    widget.pause();
   });
