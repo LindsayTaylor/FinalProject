47dc897c3e690eeb8dd03bc2def4ab9d1693aa02
LindsayTaylor <LindsayNyberg@Lindsays-MBP.home>
2015-09-01 22:18:25 -0400

    Final 1.4
    Updated Form

--- index.js
+++ index.js
@@ -2,29 +2,17 @@ $(document).ready(function(){
 
 $("#container").fadeIn(500);
 
-/*var foo = document.getElementById("noColor");*/
+/*var hasClass = element.content.contains("rainbow");*/
 
 function colorChange() {
-
-  document.getElementById("noColor").id = "space";
-
-  document.getElementById("dinoA").id="dinoB";
-
-  document.getElementById("space").style.WebkitAnimation = "change 7s infinite";
-
-  document.getElementById("space").style.animation = "change 7s infinite";
-
+  $("#noColor").toggleClass("paused");
+  $("#noColor").css("color, white");
 }
 
-function backToWhite(){
-  alert("yep");
-  /*document.getElementById("space").id = "noColor";*/
 
-}
 
 $("#noColor").click(colorChange);
-$("#space").click(backToWhite);
-/*$("#dinoB").click(backToWhite);*/
+
 
 });
 
